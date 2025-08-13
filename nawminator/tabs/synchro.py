c0789ede5bde8b6880850e03a87cabbb91c7a782
Grim <support@grimlefourbe.xyz>
2025-08-13 14:01:29 +0200

    :sparkles: Added support for parsing source code

--- nawminator/tabs/synchro.py
+++ nawminator/tabs/synchro.py
@@ -1,9 +1,14 @@
+import html
 import gradio as gr
 import nawminator as nm
 
 import pandas as pd
 import datetime as dt
 import io
+import re
+
+class ParsingError(Exception):
+    pass
 
 LOCALSTORAGE_KEY = "synchro_player_data"
 load_from_local_js = f"""(x) => {{
@@ -24,7 +29,26 @@ def synchro_tab(blocks: gr.Blocks):
         "outputs": [tab.result_df, tab.player_select, tab.target_alliance, tab.loaded_accordion],
         "js": load_from_local_js,
     }
-    
+
+copy_paste_pat = r"^([\d,]+)\s+\w+\s+(\[[-\d]+:[-\d]+\])\s+([\d,]+)\s+?([^\t]*)\s+?([^\t]+)\s+([^\s]*)\s+(?:Libre|Vassal de [^\t]+|En vacances)$"
+copy_paste_cpat = re.compile(copy_paste_pat, flags=re.MULTILINE)
+
+source_code_pat = re.compile(
+    r"""
+    <tr[^>]*>[\t \r\n]*
+    <td>[0-9,]+</td>[\t \r\n]*
+    <td[^>]*>[^<]*</td>[\t \r\n]*
+    <td>(\[[0-9:-]+\])</td>[\t \r\n]*
+    <td>([0-9,]+)</td>[\t \r\n]+
+    <td><a[^>]*>([^<]+)</a></td>[\t \r\n]*
+    <td><a[^>]+href="profil-([0-9]+)">\ <b>([^<]+)</b></a></td>[\t \r\n]*
+    <td><a[^>]*>\ <b>([^<]*)</b></a></td>[\t \r\n]*
+    <td>(?:Vassal\ de\ <a\ href='profil-)?([^<>]+)(?:'>\ <b>[^<]+</b>)?</td>[\t \r\n]*
+    </tr>
+    """, flags=re.X
+)
+
+
 class SynchroTab:
     def __init__(self):
         self.set_layout()
@@ -75,6 +99,38 @@ class SynchroTab:
             show_copy_button=False,
         )
 
+    def parse_table(self, input_data: str) -> pd.DataFrame:
+        try:
+            lines = [i.group(0) for i in copy_paste_cpat.finditer(input_data)]
+            if len(lines) == 0:
+                raise ParsingError("Found no matching line in input_data")
+            data = pd.read_table(
+                io.StringIO("\n".join(lines)),
+                names=["distance", "duration", "coord", "tdc", "colo_name", "player_name", "alliance", "status"],
+                usecols=["coord", "tdc", "colo_name", "player_name", "alliance"],
+            )
+            data = data.apply(lambda x: x.str.strip())
+            data["tdc"] = data["tdc"].apply(nm.utils.parse_naw_int)
+        except Exception as e:
+            raise ParsingError from e
+        return data
+
+    def parse_source_code(self, input_data: str) -> pd.DataFrame:
+        try:
+            data = source_code_pat.findall(input_data)
+        except Exception as e:
+            raise ParsingError from e
+        if len(data) == 0:
+            raise ParsingError("No valid lines found")
+        df = pd.DataFrame(
+            data=data, 
+            columns=["coord", "tdc", "colo_name", "profile_link", "player_name", "alliance", "status"]
+        )
+        df = df.apply(lambda x: x.str.strip())
+        df["tdc"] = df["tdc"].apply(nm.utils.parse_naw_int)
+        df["colo_name"] = df["colo_name"].apply(html.unescape)
+        return df[["coord", "tdc", "colo_name", "player_name", "alliance"]]
+
     def configure_triggers(self):
         @gr.on(
             self.data_input_btn.click,
@@ -82,17 +138,25 @@ class SynchroTab:
             outputs=self.result_df,
         )
         def parse_data(input_data: str):
-            data = pd.read_table(
-                io.StringIO(input_data),
-                names=["distance", "duration", "coord", "tdc", "colo_name", "player_name", "alliance", "status"],
-                usecols=["coord", "tdc", "colo_name", "player_name", "alliance"],
-            )
-            data = data.apply(lambda x: x.str.strip())
-            data["tdc"] = data["tdc"].apply(nm.utils.parse_naw_int)
+            print("Parsing input data")
+            exceptions = []
+            for parser in [self.parse_table, self.parse_source_code]:
+                print(f"With {parser.__name__}")
+                try:
+                    data = parser(input_data=input_data)
+                except ParsingError as e:
+                    exceptions.append(e)
+                    continue
+                print(f"No error with parser {parser.__name__}")
+                break
+            else:
+                raise ExceptionGroup("No parsing worked for the input data", exceptions)
+
             print(data.shape)
             print(data.columns)
             data[["x", "y"]] = data["coord"].str.strip("[] ").str.split(":", expand=True).astype(int)
             del data["coord"]
+            data.attrs["parsing_date"] = dt.datetime.now()
             return data[["player_name", "colo_name", "alliance", "x", "y", "tdc"]].sort_values("alliance")
         
         @gr.on(
